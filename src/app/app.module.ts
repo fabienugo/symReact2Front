e76d9d960fd021430d4b225c7aaf7114c47a9f88
Fabien Ugo <fabien.ugo@hotmail.fr>
2019-09-04 20:22:02 +0200

    Mise en place de l'architecture

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './common/components/navbar/navbar.component';
+import { SigninComponent } from './auth/signin/signin.component';
+import { SignupComponent } from './auth/signup/signup.component';
+import { HomepageComponent } from './homepage/homepage.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SigninComponent,
+    SignupComponent,
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
